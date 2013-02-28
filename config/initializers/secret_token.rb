83473f55a576c0302fac6c0ed03be516882cb5f5
Kim Lodfelt <kim.lodfelt@dynabyte.se>
2013-02-28 12:42:54 +0100

    created a rails project and added a html5-based presentation apge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LodfeltSe::Application.config.secret_token = 'd71c0735f944771a54260b26dafdb5e6d245ad17dbb941d9aad4482bfc2be3b6ebc294736621bd3371b98125a6c115a80ee4318dfee43a8e6793488b135443db'
